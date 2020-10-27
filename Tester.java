985f01198be4bc02734eae1e353fc601768d33f4
jliu10 <jliu10@stuy.edu>
2020-10-27 18:09:06 -0400

    tested get methods

--- Tester.java
+++ Tester.java
@@ -1,5 +1,7 @@
 public class Tester{
   public static void main(String[] args){
-    
+    BankAccount justin = new BankAccount(999,"pass");
+    System.out.println(justin.getBalance());
+    System.out.println(justin.getAccountID());
   }
 }
