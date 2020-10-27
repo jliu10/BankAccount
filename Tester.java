5072a8494aa2c299dfb9f126df56aae48d02d2ba
jliu10 <jliu10@stuy.edu>
2020-10-27 18:30:06 -0400

    tested deposit

--- Tester.java
+++ Tester.java
@@ -1,8 +1,13 @@
 public class Tester{
   public static void main(String[] args){
     BankAccount justin = new BankAccount(999,"pass");
+    BankAccount shmerp = new BankAccount(123,"shmerp4life");
     System.out.println(justin.getBalance());
     System.out.println(justin.getAccountID());
     justin.setPassword("newPass");
+    System.out.println(justin.deposit(10.50));
+    System.out.println(justin.getBalance());
+    System.out.println(shmerp.deposit(-0.1));
+    System.out.println(shmerp.getBalance());
   }
 }
