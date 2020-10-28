006f7d1014bc2ea233e8f10da01437d3e46bd7d0
jliu10 <jliu10@stuy.edu>
2020-10-28 14:15:26 -0400

    created method authenticate

--- BankAccount.java
+++ BankAccount.java
@@ -7,28 +7,42 @@ public class BankAccount{
     password = p;
     balance = 0;
   }
+
   public double getBalance(){
     return balance;
   }
+
   public int getAccountID(){
     return accountID;
   }
+
   public void setPassword(String p){
     password = p;
   }
+
   public boolean deposit(double amount){
     if(amount<0) return false;
     balance+=amount;
     return true;
   }
+
   public boolean withdraw(double amount){
     if(amount<0 || amount>balance) return false;
     balance-=amount;
     return true;
   }
+
   public String toString(){
     String result="";
     result+="#"+accountID+"\t$"+balance;
     return result;
+  }
+
+  private boolean authenticate(String password){
+    return password.equals(this.password);
+  }
+
+  public boolean transferTo(BankAccount other, double amount, String password){
+
   }
 }
